05a85314dba2834a9f0e779141cc9002b2091254
Carl Corsini <carl.m.corsini@gmail.com>
2018-11-29 11:52:31 -0800

    complete Input component render tests for success=false

---
+++ src/Input.test.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+
+import { findByTestAttr, storeFactory } from '../test/testUtils'
+import Input from './Input'
+
+const setup = (initialState = {}) => {
+  const store = storeFactory(initialState)
+  const wrapper = shallow(<Input store={store} />).dive()
+  return wrapper
+}
+
+setup()
+
+describe('render', () => {
+  describe('word HAS NOT been guessed', () => {
+    let wrapper
+    beforeEach(() => {
+      const initialState = { success: false }
+      wrapper = setup(initialState)
+    })
+    test('renders component without error', () => {
+      const component = findByTestAttr(wrapper, 'component-input')
+      expect(component.length).toBe(1)
+    })
+    test('renders input box', () => {
+      const inputBox = findByTestAttr(wrapper, 'input-box')
+      expect(inputBox.length).toBe(1)
+    })
+    test('renders submit button', () => {
+      const submitButton = findByTestAttr(wrapper, 'submit-button')
+      expect(submitButton.length).toBe(1)
+    })
+  })
+  describe('word HAS been guessed', () => {
+    test('renders component without error', () => {})
+    test('DOES NOT render input box', () => {})
+    test('DOES NOT render submit button', () => {})
+  })
+})
+describe('update state', () => {})
