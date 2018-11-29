55f15441f8a923ead946ab00bc19f9e14bcfd804
Carl Corsini <carl.m.corsini@gmail.com>
2018-11-29 11:36:26 -0800

    Set up Input component and storeFactory test util

--- test/testUtils.js
+++ test/testUtils.js
@@ -1,4 +1,17 @@
 import checkPropTypes from 'check-prop-types'
+import { createStore } from 'redux'
+import rootReducer from '../src/reducers'
+
+/**
+ * create a testing store with imported reducers, middleware, and initial state.
+ * globals: rootReducer
+ * @param  {object} initialState - initial state for store
+ * @function storeFactory
+ * @return {Store} - Redux store
+ */
+export const storeFactory = initialState => {
+  return createStore(rootReducer, initialState)
+}
 
 /**
  * Return nodes with given data-test attribute.
