769ee9ef4b4ea9cdfdb7103b157a380472540f3d
Carl Corsini <carl.m.corsini@gmail.com>
2018-12-12 13:37:49 -0800

    add getSecretword actio creator on app mount

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ import Congrats from './Congrats'
 import Input from './Input'
 import { getSecretWord } from './actions'
 
-class App extends Component {
+export class UnconnectedApp extends Component {
+  componentDidMount() {
+    this.props.getSecretWord()
+  }
   render() {
     return (
       <div className="container">
@@ -27,4 +30,4 @@ const mapStateToProps = state => {
 export default connect(
   mapStateToProps,
   { getSecretWord }
-)(App)
+)(UnconnectedApp)
