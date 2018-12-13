d37254e184ea44a662f6560f68677c018aef7d99
Carl Corsini <carl.m.corsini@gmail.com>
2018-12-13 15:15:56 -0800

    custom disses and grats

--- src/Congrats.js
+++ src/Congrats.js
@@ -9,13 +9,29 @@ import FadeIn from 'react-fade-in'
  * @return {[JSX.element]} Render component or 'null' if success prop is false
  */
 
+const randomQuote = (array, max) => {
+  let random = Math.floor(Math.random() * Math.floor(max))
+  return array[random]
+}
+
+const phrase = [
+  'a gosh darn genius!',
+  'a savant.',
+  'the smartest person in the world!',
+  'destined for greatness!',
+  'insane in the membrane!',
+  'my best friend :)',
+  'pretty smart.',
+]
+
 const Congrats = props => {
   if (props.success) {
+    let grats = randomQuote(phrase, phrase.length)
     return (
       <FadeIn>
         <div className="alert alert-success" data-test="component-congrats">
           <span data-test="congrats-message">
-            Congratulations, You're an mf geniusss
+            Congratulations, You're {grats}
           </span>
           <br />
           <button
--- src/GiveUp.js
+++ src/GiveUp.js
@@ -9,13 +9,32 @@ import FadeIn from 'react-fade-in'
  * @return {[JSX.element]} Render component or 'null' if success prop is false
  */
 
+const randomQuote = (array, max) => {
+  let random = Math.floor(Math.random() * Math.floor(max))
+  return array[random]
+}
+
+const phrase = [
+  "'re dumb",
+  "'re not smart",
+  "'re weak willed",
+  "'re a quitter",
+  "'re a disappointment",
+  "'re not my friend anymore",
+  "'re not the sharpest tool in the shed",
+  ' bring shame upon your family',
+]
+
 const GiveUp = props => {
   if (props.giveUp) {
+    let diss = randomQuote(phrase, phrase.length)
+
     return (
       <FadeIn>
         <div className="alert alert-danger" data-test="component-give-up">
           <span data-test="give-up-message">
-            You're weak, the secret word was <em>{props.secretWord}</em>
+            You
+            {diss}, the secret word was <em>{props.secretWord}</em>
           </span>
           <br />
           <button
