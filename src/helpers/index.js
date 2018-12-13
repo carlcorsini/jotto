0aa2d5dbf233adaeee2dddefca6bcbdbefc9f99a
Carl Corsini <carl.m.corsini@gmail.com>
2018-12-13 13:27:22 -0800

    removed console log

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -5,7 +5,6 @@
  * @return {number} - Number of letters matched between guessed word and secret word
  */
 export function getLetterMatchCount(guessedWord, secretWord) {
-  console.log(secretWord, guessedWord)
   const secretLetterSet = new Set(secretWord.split(''))
   const guessedLetterSet = new Set(guessedWord.split(''))
   return [...secretLetterSet].filter(letter => guessedLetterSet.has(letter))
