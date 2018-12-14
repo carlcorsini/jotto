776ccac2f3eb06a388b7e52d667142f8f47a4eb8
Carl Corsini <carl.m.corsini@gmail.com>
2018-12-13 16:30:17 -0800

    log

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -5,7 +5,6 @@
  * @return {number} - Number of letters matched between guessed word and secret word
  */
 export function getLetterMatchCount(guessedWord, secretWord) {
-  console.log(secretWord)
   const secretLetterSet = new Set(secretWord.toLowerCase().split(''))
   const guessedLetterSet = new Set(guessedWord.toLowerCase().split(''))
   return [...secretLetterSet].filter(letter => guessedLetterSet.has(letter))
